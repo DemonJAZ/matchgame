55e8c1655012eb6f0d677457e205c6b2f38da39b
Jaspreet Mahal <jpjaspreetmahal@Jaspreets-MacBook-Pro.local>
2017-12-06 13:46:58 +0530

    Timer and star functionality added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,101 @@
 /*
  * Create a list that holds all of your cards
  */
+var array = document.getElementsByClassName("card");
+var stars = document.getElementsByClassName("fa fa-star");
+var starCount = stars.length-1;
+var movesCount = document.getElementById("moves");
+movesCount.innerHTML = 0;
+var newarray = shuffle(array);
+
+for (var i = 0; i < array.length; i++) {
+  array[i].replaceWith(newarray[i]);
+}
+
+var firstCardOpened = false;
+var firstCard,secondCard;
+var count = 0;
+var winCount = 0;
+for (var i = 0; i < array.length; i++) {
+    array[i].addEventListener('click',function(){
+    count = count + 1;
+    movesCount.textContent = count;
+    if (starCount > 0) {    // change star counts
+      if(count===18)
+      {
+         stars[starCount--].remove();
+      }
+      else if(count==24)
+      {
+         stars[starCount--].remove();
+      }
+    }
+
+    this.className = "card open show";
+    if(!firstCardOpened)
+    {
+      firstCard = this;
+      firstCardOpened = true;
+    }else{
+      secondCard = this;
+      if(firstCard.childNodes[1].className === secondCard.childNodes[1].className) // check if cards match
+      {
+        secondCard.className = "card match";
+        firstCard.className = "card match";
+        firstCardOpened = false;
+        firstCard = 'undefined';
+        secondCard = 'undefined';
+        winCount = winCount+2;
+        if(winCount === 16) // Win condition Game Over
+        {
+           var performance;
+           var timeTaken = seconds;
+           if(starCount === 3)
+           {
+             performance = "Exellent!!";
+           }else if (starCount === 2) {
+             performance = "Good!!";
+           }else{
+             performance = "you can do better";
+           }
+
+           alert("WON!! \n Performance =" + performance + "\n Time Taken:" + timeTaken + "secs");
+           reset();
+        }
+      }
+      else{  // reset th cards if not match
+        secondCard.className="card open show";
+        setTimeout(function() {
+          secondCard.className="card";
+          firstCard.className = "card";
+          firstCardOpened = false;
+          firstCard = 'undefined';
+          secondCard = 'undefined';
+        },200);
+      }
+    }
+  });
+}
 
 
+
+// reset grid
+function reset(){
+  array = shuffle(array);
+  for (var i = 0; i < array.length; i++) {
+    array[i].className = "card";
+  }
+  seconds = 0;
+  winCount = 0;
+  count = 0;
+  movesCount.innerHTML = 0;
+  while(stars.length<3)
+  {
+    var a = stars[stars.length-1].cloneNode(true);
+    stars[stars.length-1].append(a);
+  }
+  starCount = stars.length - 1;
+}
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -24,6 +117,14 @@ function shuffle(array) {
 
     return array;
 }
+
+// Timer
+var seconds = 0;
+setInterval(function(){
+  var sec = document.getElementById('seconds');
+  sec.innerHTML = seconds;
+  seconds = seconds + 1;
+},1000)
 
 
 /*
